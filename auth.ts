ecab150b7752d60c5d85877872e768586386ae35
M K Mandal <mkuser608@gmail.com>
2024-12-29 09:33:47 +0530

    new verification token

--- auth.ts
+++ auth.ts
@@ -21,6 +21,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   callbacks: {
     async signIn({ user, account }) {
+      console.log({
+        user,
+        account,
+      });
       //Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
       const existingUser = await getUserById(user.id as string);
